06960b0761825a2bc44d8f888c83ed6e2f3deed7
Yash9617 <yashasvi.sharma@rajasoftwarelabs.com>
2023-10-21 17:43:09 +0530

    Fixed gradle issues

--- app/src/main/java/com/example/movieapp/ui/MovieActivity.kt
+++ app/src/main/java/com/example/movieapp/ui/MovieActivity.kt
@@ -2,9 +2,9 @@ package com.example.movieapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.movieapp.R
 import com.example.movieapp.databinding.ActivityMovieBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,8 +14,8 @@ class MovieActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMovieBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie)
         binding = ActivityMovieBinding.inflate(layoutInflater)
-        binding.bottomNavigationView.setupWithNavController(binding.fragmentContainer.findNavController())
+        setContentView(binding.root)
+        binding.bottomNavigationView.setupWithNavController(binding.fragmentContainer[0].findNavController())
     }
 }
